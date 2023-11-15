ebde2b451f0c445b6462ce0605266a493f33fb41
naktus-kouki <k-natus@digeon.co>
2023-11-15 05:27:03 +0000

    fix method handle

--- api/router/handle_method.go
+++ api/router/handle_method.go
@@ -2,6 +2,31 @@ package router
 
 import "net/http"
 
+type (
+	MethodRoute struct {
+		httpMethod  HTTPMethod
+		handlerFunc http.HandlerFunc
+	}
+
+	HandleHTTPMethod struct {
+		mux             *http.ServeMux
+		handlerWrappers []HandlerWrapper
+	}
+
+	HandlerWrapper func(next http.Handler) http.HandlerFunc
+)
+
+func NewHandleHTTPMethod(mux *http.ServeMux, handlerWrappers []HandlerWrapper) *HandleHTTPMethod {
+	return &HandleHTTPMethod{
+		mux,
+		handlerWrappers,
+	}
+}
+
+func (h *HandleHTTPMethod) Add(url string, methodRoutes []MethodRoute) {
+	handleMethod(h, url, methodRoutes)
+}
+
 // This function invokes middleware and allows a specific HTTP method.
 func handleMethod(h *HandleHTTPMethod, url string, methodRoutes []MethodRoute) {
 	h.mux.HandleFunc(
